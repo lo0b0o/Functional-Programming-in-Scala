8b3c52134da45b18b764ee864d7158f058f7cfa4
tchx <tctsui@outlook.com>
2024-07-21 17:52:33 +0200

    1-1 assignment done

--- 1-Functional-Programming-Principles-in-Scala/progfun1-example/example/src/main/scala/example/Lists.scala
+++ 1-Functional-Programming-Principles-in-Scala/progfun1-example/example/src/main/scala/example/Lists.scala
@@ -22,7 +22,9 @@ object Lists:
    * @param xs A list of natural numbers
    * @return The sum of all elements in `xs`
    */
-  def sum(xs: List[Int]): Int = ???
+  def sum(xs: List[Int]): Int = 
+    if(xs.isEmpty) 0
+    else xs.head + sum(xs.tail)
 
   /**
    * This method returns the largest element in a list of integers. If the
@@ -37,4 +39,12 @@ object Lists:
    * @return The largest element in `xs`
    * @throws java.util.NoSuchElementException if `xs` is an empty list
    */
-  def max(xs: List[Int]): Int = ???
+  def max(xs: List[Int]): Int = 
+    if(xs.isEmpty) throw new java.util.NoSuchElementException()
+    if(xs.tail.isEmpty) xs.head
+    else {
+      var m=max(xs.tail)
+      if (xs.head>m) xs.head
+      else m
+    }
+
---
+++ 1-Functional-Programming-Principles-in-Scala/progfun1-example/example/src/main/scala/main.scala
@@ -0,0 +1,7 @@
+package example
+
+@main def runMax(): Unit =
+  println(Lists.max(List(1, 3, 2)))
+
+@main def runSum(): Unit =
+  println(Lists.sum(List(1, 3, 2)))
